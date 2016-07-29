c0f2308d3fe8de78d970027d140459e299e5edc4
David Ellsworth <david@worms2d.info>
2016-07-29 09:34:22 -0700

    [internal] Remove an unused macro
    This should have been done in aab572d6217f0ed5bbab111188538acc80f269cb

--- macros.h
+++ macros.h
@@ -9,8 +9,6 @@ template <size_t size>
 char (*__strlength_helper(char const (&_String)[size]))[size];
 #define strlength(_String) (sizeof(*__strlength_helper(_String))-1)
 
-#define STRING_WITH_LEN(s) s, strlength(s)
-
 #ifndef _countof
 #define _countof(array) (sizeof(array)/sizeof(array[0]))
 #endif

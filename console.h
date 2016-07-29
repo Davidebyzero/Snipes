Davidebyzero/Snipes

commit 0cbd9a439c89302abbea6a9ac7ebef2ccca241df

Remove all usage of Windows.h in Snipes.cpp and header files
Enforce encapsulation of Windows-specific implementation details
by not including Windows.h anywhere in Snipes.cpp or its includes.

Summary of changes:
- Add more Windows numeric types to types.h
- Move implementation of WriteTextToConsole template to its own function
  in Console.cpp
- Encapsulate Windows Sleep function into new SleepMilliseconds function
  in timer.h/.cpp
- Define _countof when it is not defined
- Replace usage of FOREGROUND_* constants in Snipes.cpp
- Replace usage of MAX_PATH is Snipes.cpp

console.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <Windows.h>
 #include "types.h"
 #include "macros.h"
 
@@ -13,13 +12,9 @@ void EraseBottomTwoLines();
 DWORD ReadTextFromConsole(char buffer[], DWORD bufsize);
 
 void SetConsoleOutputTextColor(WORD wAttributes);
-template <size_t LENGTH>
-void WriteTextToConsole(char const (&text)[LENGTH])
-{
-	extern HANDLE output;
-	DWORD numwritten;
-	WriteConsole(output, text, strlength(text), &numwritten, 0);
-}
+
+void WriteTextToConsole(char const *text, size_t length);
+template <size_t LENGTH> void WriteTextToConsole(char const (&text)[LENGTH]) { WriteTextToConsole(text, LENGTH); }
 
 void OpenDirectConsole();
 void CloseDirectConsole(Uint lineNum);

Snipes.cpp
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <io.h>
 #include <wchar.h>
+#include <string.h>
+#include <math.h>
 #include "config.h"
 #include "Snipes.h"
 #include "types.h"
@@ -2208,7 +2210,7 @@ int __cdecl main(int argc, char* argv[])
 			struct tm *rectime_gmt;
 			rectime_gmt = gmtime(&rectime);
 
-			char replayFilename[MAX_PATH];
+			char replayFilename[1024];
 			sprintf(replayFilename,
 					"%04d-%02d-%02d %02d.%02d.%02d.SnipesGame",
 					1900+rectime_gmt->tm_year, rectime_gmt->tm_mon+1, rectime_gmt->tm_mday,
@@ -2321,7 +2323,7 @@ int __cdecl main(int argc, char* argv[])
 			{
 				for (;;)
 				{
-					Sleep(1);
+					SleepMilliseconds(1);
 					WORD tick_count2 = GetTickCountWord();
 					if (tick_count2 != tick_count)
 					{

Snipes.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <Windows.h>
 #include "config.h"
 #include "types.h"
 

console.cpp
@@ -135,6 +135,12 @@ void SetConsoleOutputTextColor(WORD wAttributes)
 	SetConsoleTextAttribute(output, wAttributes);
 }
 
+void WriteTextToConsole(char const *text, size_t length)
+{
+	DWORD numwritten;
+	WriteConsole(output, text, length, &numwritten, 0);
+}
+
 void OpenDirectConsole()
 {
 	SetConsoleMode(output, 0);

macros.h
@@ -10,3 +10,7 @@ char (*__strlength_helper(char const (&_String)[size]))[size];
 #define strlength(_String) (sizeof(*__strlength_helper(_String))-1)
 
 #define STRING_WITH_LEN(s) s, strlength(s)
+
+#ifndef _countof
+#define _countof(array) (sizeof(array)/sizeof(array[0]))
+#endif

timer.cpp
@@ -22,3 +22,8 @@ int OpenTimer()
 void CloseTimer()
 {
 }
+
+void SleepMilliseconds(DWORD dwMsecs)
+{
+	Sleep(dwMsecs);
+}

timer.h
@@ -1,9 +1,10 @@
 #pragma once
 
-#include <Windows.h>
 #include "types.h"
 
 WORD GetTickCountWord();
 
 int  OpenTimer();
 void CloseTimer();
+
+void SleepMilliseconds(DWORD dwMsecs);

types.h
@@ -1,6 +1,8 @@
 #pragma once
 
 typedef unsigned char       BYTE;
+typedef          short      SHORT;
 typedef unsigned short      WORD;
 typedef unsigned int        Uint;
+typedef unsigned long       DWORD;
 typedef unsigned long long  QWORD;

commit 40c8ae55de2fdff73e3e135d0accdfff5e396cf7

Fix length passed to WriteTextToConsole()
Bug introduced in 0cbd9a439c89302abbea6a9ac7ebef2ccca241df

console.h
@@ -14,7 +14,7 @@ DWORD ReadTextFromConsole(char buffer[], DWORD bufsize);
 void SetConsoleOutputTextColor(WORD wAttributes);
 
 void WriteTextToConsole(char const *text, size_t length);
-template <size_t LENGTH> void WriteTextToConsole(char const (&text)[LENGTH]) { WriteTextToConsole(text, LENGTH); }
+template <size_t LENGTH> void WriteTextToConsole(char const (&text)[LENGTH]) { WriteTextToConsole(text, strlength(text)); }
 
 void OpenDirectConsole();
 void CloseDirectConsole(Uint lineNum);
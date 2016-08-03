Davidebyzero/Snipes

commit b369e7b6a34947a902d2fe90de39910f4e553c9f

Fix compilation on Linux with GCC
First commit compiling (not linking) Snipes.cpp on Linux.

Snipes.cpp
@@ -1,8 +1,8 @@
 #include <time.h>
 #include <stdio.h>
-#include <io.h>
 #include <wchar.h>
 #include <string.h>
+#include <stdlib.h>
 #include <math.h>
 #include "config.h"
 #include "Snipes.h"
@@ -12,6 +12,7 @@
 #include "timer.h"
 #include "sound.h"
 #include "keyboard.h"
+#include "platform.h"
 
 bool got_ctrl_break = false;
 bool forfeit_match = false;

platform.h
@@ -1,0 +1,11 @@
+// Compiler shims
+
+#if defined(_MSC_VER)
+
+#elif defined(__GNUC__)
+
+#define __debugbreak abort
+#define __assume(cond) do { if (!(cond)) __builtin_unreachable(); } while (0)
+#define __cdecl
+
+#endif

GNUmakefile
@@ -1,0 +1,5 @@
+SOURCES=$(addsuffix .cpp, Snipes) # console keyboard sound timer
+CFLAGS=-fpermissive
+
+snipes : $(SOURCES)
+	gcc -o $@ $(CFLAGS) $(SOURCES)

commit 6c3d4916d75ad5d0c5739fe15bbca3aa9337c0e5

Fix compiling with CHEAT enabled
Bug introduced in b369e7b6a34947a902d2fe90de39910f4e553c9f

Snipes.cpp
@@ -2285,8 +2285,8 @@ extern "C" int __cdecl SDL_main(int argc, char* argv[])
 			{
 				skip_to_frame = 0;
 				playbackMode = false;
-				long fileSize = ftell(replayFile);
-				_chsize(_fileno(replayFile), fileSize);
+				auto fileSize = ftell(replayFile);
+				changesize(_fileno(replayFile), fileSize);
 				BYTE dummy;
 				size_t blah = fread(&dummy, 1, 1, replayFile);
 				fseek(replayFile, fileSize, SEEK_SET);

platform.h
@@ -2,12 +2,16 @@
 
 #if defined(_MSC_VER)
 
+#include <io.h>
 #define UNREACHABLE __assume(0)
+#define changesize(fd,size) _chsize(fd,size)
 
 #elif defined(__GNUC__)
 
+#include <unistd.h>
 #define __debugbreak abort
 #define UNREACHABLE __builtin_unreachable()
+#define changesize(fd,size) ftruncate(fd,size)
 
 #endif
 
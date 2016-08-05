Davidebyzero/Snipes

commit 3712d429e63e85dadfa994a33ea743365e0a6fec

GNUmakefile: Add "make clean" option

GNUmakefile
@@ -5,3 +5,6 @@ CXX=g++ # CXX="clang++"
 
 snipes : $(SOURCES) config.h GNUmakefile
 	$(CXX) -o $@ $(CFLAGS) $(SOURCES) $(LDFLAGS)
+
+clean :
+	rm -f snipes

commit 146417004505760efac03431abed2eab3c3776ad

GNUmakefile: Make line endings consistent
Inconsistency introduced in 3712d429e63e85dadfa994a33ea743365e0a6fec

Snipes.cpp
@@ -1703,6 +1703,8 @@ void EraseObjectFromMaze()
 	}
 }
 
+//template <typename TYPE> TYPE &IncWrap(TYPE &n, 
+
 void UpdateGenerators()
 {
 	for (ObjectIndex object = objectHead_generators; object;)
594af45108e07aa4159c3babc9b5e53609c3fd6e
Vladimir Panteleev <git@thecybershadow.net>
2017-12-08 00:15:29 +0000

    [SDL] Request an accelerated renderer with VSync
    VSync will improve screen tearing, and by locking drawing to the
    screen vertical refresh, can improve CPU usage on some systems.

--- sdl/console.cpp
+++ sdl/console.cpp
@@ -316,7 +316,7 @@ static int SDLCALL ConsoleThreadFunc(void*)
 		return 1;
 	}
 
-	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, 0);
+	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (!ren)
 	{
 		SDL_DestroyWindow(win);
